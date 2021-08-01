9e4fbd3b997fc77725b9878d09f88a1657ea26be
Tucker Andrea <tandrea@pr-i6.kzn.21-school.ru>
2021-08-01 18:08:32 +0300

    Additing ft_check_right

--- ex00/ft_check.c
+++ ex00/ft_check.c
@@ -6,7 +6,7 @@
 /*   By: alcierra <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/01 17:30:14 by alcierra          #+#    #+#             */
-/*   Updated: 2021/08/01 17:46:50 by alcierra         ###   ########.fr       */
+/*   Updated: 2021/08/01 18:07:18 by tandrea          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,13 +23,36 @@ int	ft_check_left(int *arr, int num)
 		j = 0;
 		while (j < i)
 		{
-			if (arr[i] < arr[j])
+			if (arr[i] <= arr[j])
 				break ;
 			if (j == i - 1)
 				count++;
 			j++;
 		}
 		i++;
+	}
+	return (count);
+}
+ int ft_check_right (int *arr, int num)
+{
+	int	i;
+	int	j;
+	int	count;
+
+	count = 1;
+	i = num - 1;
+	while (i >= 0)
+	{
+		j = num - 1;
+		while (j > i)
+		{
+			if (arr[i] <= arr[j])
+				break ;
+			if (i == j - 1)
+				count++;
+			j--;
+		}
+		i--;
 	}
 	return (count);
 }
