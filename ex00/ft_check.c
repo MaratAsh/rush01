89745a813e717c31b5994dc97f019aa8ad117f3a
Tucker Andrea <tandrea@pr-i6.kzn.21-school.ru>
2021-08-01 20:00:36 +0300

    Additing checkers

--- ex00/ft_check.c
+++ ex00/ft_check.c
@@ -6,7 +6,7 @@
 /*   By: alcierra <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/01 17:30:14 by alcierra          #+#    #+#             */
-/*   Updated: 2021/08/01 18:07:18 by tandrea          ###   ########.fr       */
+/*   Updated: 2021/08/01 19:59:10 by tandrea          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,7 +33,8 @@ int	ft_check_left(int *arr, int num)
 	}
 	return (count);
 }
- int ft_check_right (int *arr, int num)
+
+int ft_check_right(int *arr, int num)
 {
 	int	i;
 	int	j;
@@ -53,6 +54,66 @@ int	ft_check_left(int *arr, int num)
 			j--;
 		}
 		i--;
+	}
+	return (count);
+}
+
+int	ft_check_up(int **matrix, int num)
+{
+	int	i1;
+	int	i2;
+	int	j;
+	int	count;
+	
+	i1 = 0;
+	count = 1;
+	while (i1 < num)
+	{
+		j = 0;
+		while (j < num)
+		{
+			i2 = 0;
+			while (i2 <= i1)
+			{
+				if (matrix[i1][j] <= matrix[i2][j])
+					break ;
+				if (i2 == i1 - 1)
+					count++;
+				i2++;
+			}
+			j++;
+		}
+		i1++;
+	}
+	return (count);
+}
+
+int	ft_check_down(int **matrix, int num)
+{
+	int	i1;
+	int	i2;
+	int	j;
+	int	count;
+
+	i1 = num - 1;
+	count = 1;
+	while (i1 < num)
+	{
+		j = 0;
+		while (j < num)
+		{
+			i2 = num - 1;
+			while (i2 >= i1)
+			{
+				if (matrix[i1][j] <= matrix[i2][j])
+					break ;
+				if (i1 == i2 - 1)
+					count++;
+				i2--;
+			}
+			j++;
+		}
+		i1--;
 	}
 	return (count);
 }
