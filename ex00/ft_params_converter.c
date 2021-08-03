cd8020dd140307426f71cc2d6cb02de40674681e
Altagracia <alcierra@student.21-school.ru>
2021-08-03 15:33:07 +0300

    Changing problem with incorrect number of values

--- ex00/ft_params_converter.c
+++ ex00/ft_params_converter.c
@@ -6,7 +6,7 @@
 /*   By: alcierra <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/01 10:27:06 by alcierra          #+#    #+#             */
-/*   Updated: 2021/08/01 13:24:41 by alcierra         ###   ########.fr       */
+/*   Updated: 2021/08/03 15:32:17 by alcierra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ int	*ft_params_converter(char *str)
 		i = 0;
 		while (i < 16)
 		{
-			if (str[i * 2] >= '0' && str[i * 2] <= '4')
+			if (str[i * 2] >= '1' && str[i * 2] <= '4')
 			{
 				arr[i] = str[i * 2] - '0';
 			}
@@ -35,5 +35,7 @@ int	*ft_params_converter(char *str)
 			i++;
 		}
 	}
+	if (arr[i] != '\0')
+		return (NULL);
 	return (arr);
 }
